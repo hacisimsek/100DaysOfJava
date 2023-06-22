b016f83126e231f700a4125336cbda522c46fed8
Hacı Şimşek <haci.simsek@sisal.com>
2023-06-22 23:36:32 +0300

    Day_1 commit Generate Random Number Between 1 and 10

---
+++ Day_1/src/Main.java
@@ -0,0 +1,15 @@
+import java.security.SecureRandom;
+
+public class Main {
+    public static final SecureRandom random = new SecureRandom();
+
+    public static int randomInt(int min, int max) {
+        return random.nextInt(max - min + 1) + min;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Generating random numbers between 1 and 10");
+        System.out.println("==========================================");
+        System.out.println("Random number: " + randomInt(1, 10));
+    }
+}
