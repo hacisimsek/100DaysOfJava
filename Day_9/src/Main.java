67893564713232409b51d3f01a7212eb6cc53b3f
Hacı Şimşek <haci.simsek@sisal.com>
2023-06-24 10:39:50 +0300

    Day 9 - Creating a valued Enum.

---
+++ Day_9/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        for ( Gender gender : Gender.values()){
+            System.out.printf("The value of %s is %s%n", gender ,gender.getValue());
+        }
+    }
+    public enum Gender{
+        FEMALE('f'),
+        MALE('m');
+
+        private final char value;
+
+        Gender(char value){
+            this.value = value;
+        }
+        public char getValue(){
+            return value;
+        }
+    }
+}
