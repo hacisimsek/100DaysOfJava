8dd7a25ec7c967878a6b263c93b587140a782d53
Hacı Şimşek <haci.simsek@sisal.com>
2023-07-27 22:04:07 +0300

    Day 10 - Using Google’s Jimfs to write tests that use an in-memory file system.

--- Day010/src/main/java/com/hacisimsek/day_10/Day10Application.java
+++ Day010/src/main/java/com/hacisimsek/day_10/Day10Application.java
@@ -2,7 +2,7 @@ package com.hacisimsek.day_10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Day 10 - Using Google’s Jimfs to write tests that use an in-memory file system.
 @SpringBootApplication
 public class Day10Application {
 
