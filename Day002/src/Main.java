3bbbe7785758721580060ed05ad65b90845367c0
Hacı Şimşek <haci.simsek@sisal.com>
2023-07-27 21:57:08 +0300

    Day 2 - Formatting a LocalDateTime object.

--- Day002/src/Main.java
+++ Day002/src/Main.java
@@ -1,6 +1,6 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+//Day 2 - Formatting a LocalDateTime object.
 public class Main {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     public static void main(String[] args) {
