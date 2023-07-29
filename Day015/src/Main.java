0776b1b9cc1014a9cc4182d7820cce5b481564c7
Hacı Şimşek <haci.simsek@sisal.com>
2023-07-29 21:33:24 +0300

    Day 15 - Builder Pattern

---
+++ Day015/src/Main.java
@@ -0,0 +1,90 @@
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+        Person john = Person.builder()
+                .name("John")
+                .children(Collections.singletonList(
+                        Person.builder()
+                                .name("Amanda")
+                                .petName("Toto")
+                                .build()
+                ))
+                .build();
+
+        System.out.println(john);
+    }
+
+    public static class Person {
+
+        private final String name;
+
+        private final List<Person> children;
+
+        private final String petName;
+
+        private Person(Builder builder) {
+            name = Objects.requireNonNull(builder.name);
+            children = builder.children != null ? builder.children : Collections.emptyList();
+            petName = builder.petName;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public List<Person> getChildren() {
+            return children;
+        }
+
+        public String getPetName() {
+            return petName;
+        }
+
+        public static Builder builder() {
+            return new Builder();
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", children=" + children +
+                    ", petName='" + petName + '\'' +
+                    '}';
+        }
+    }
+
+    public static final class Builder {
+
+        private String name;
+
+        private List<Person> children;
+
+        private String petName;
+
+        private Builder() {
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder children(List<Person> children) {
+            this.children = Collections.unmodifiableList(children);
+            return this;
+        }
+
+        public Builder petName(String petName) {
+            this.petName = petName;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+}
