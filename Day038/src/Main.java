8c84e927dd8599ca3baaa993eba5a5b297437cae
Hacı Şimşek <haci.simsek@sisal.com>
2023-11-06 23:46:52 +0300

    Day 37 - Filtering a Stream using Predicate#not.

---
+++ Day038/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import static java.util.function.Predicate.not;
+
+public class Main {
+    public static void main(String[] args) {
+        // Instead of doing:
+        printAllThat(word -> !word.isEmpty());
+
+        // Just do:
+        printAllThat(not(String::isEmpty));
+    }
+    private static void printAllThat(Predicate<String> filter) {
+        Stream.of("avocado", "chair", "", "dog", "car")
+                .filter(filter)
+                .forEach(System.out::println);
+    }
+}
