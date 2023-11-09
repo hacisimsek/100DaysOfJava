23fd20aae1239b25810fedee9e2e555c804ba238
Hacı Şimşek <haci.simsek@sisal.com>
2023-11-09 16:28:28 +0300

    Day 41 - Formatting Strings with java.lang.String#formatted.

---
+++ Day041/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static final String NAME = "Helber Belmiro";
+    public static void main(String[] args) {
+        String formattedString;
+
+        // Instead of doing:
+        formattedString = String.format("My name is %s", NAME);
+
+        // Just do: (Requires JDK 15)
+        formattedString = "My name is %s".formatted(NAME);
+
+        System.out.println(formattedString);
+    }
+}
