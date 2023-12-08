61398e724f280990c8c6ed553d9db43accfcad74
Hacı Şimşek <haci.simsek@sisal.com>
2023-12-08 09:10:45 +0300

    Day 68 - Verifying if a field is annotated by a particular annotation.

---
+++ Day068/src/Main.java
@@ -0,0 +1,44 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public class Main {
+
+    public static void main(String[] args) throws NoSuchFieldException {
+        var myObject = new MyClass(2021, 8);
+
+        System.out.println(isAnnotated(myObject, "year"));   // Prints: true
+        System.out.println(isAnnotated(myObject, "month"));   // Prints: true
+    }
+
+    private static boolean isAnnotated(Object object, String fieldName) throws NoSuchFieldException {
+        return object.getClass().getDeclaredField(fieldName).isAnnotationPresent(MyAnnotation.class);
+    }
+
+    static class MyClass {
+
+        @MyAnnotation
+        private final int year;
+
+        private final int month;
+
+        MyClass(int year, int month) {
+            this.year = year;
+            this.month = month;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public int getMonth() {
+            return month;
+        }
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.FIELD)
+    @interface MyAnnotation {
+    }
+}
